061b7c57d64dd570254b6fd75d66df4cee4d4735
Hugo <hugoomdra@gmail.com>
2021-04-10 19:26:27 +0200

    ajout du modèle question, de l'activity et des flags

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.quizapp
+
+object Constants{
+
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>();
+
+        val que1 = Question(1, "A quel pays appartient ce drapeau ?",
+        R.drawable.ic)
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN; //cette ligne permet de mettre l'application en full screen
+
+        btn_start.setOnClickListener{
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Entrez votre nom s'il vous plaît", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
