3f8dc99488da569ca404124fafe230ea93feb017
Hugo <hugoomdra@gmail.com>
2021-04-10 19:59:24 +0200

    Ajout de l'UI des questions

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -5,8 +5,19 @@ object Constants{
     fun getQuestions(): ArrayList<Question>{
         val questionsList = ArrayList<Question>();
 
-        val que1 = Question(1, "A quel pays appartient ce drapeau ?",
-        R.drawable.ic)
+        val que1 = Question(
+            1,
+            "A quel pays appartient ce drapeau ?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentine",
+            "Angleterre",
+            "Inde",
+            "France",
+            1
+        )
+
+        questionsList.add(que1);
+        return questionsList;
     }
 
 
--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,10 +2,29 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionList = Constants.getQuestions();
+
+        val currentPosition = 1;
+        val question: Question? = questionList[currentPosition - 1];
+
+        progressBar.progress = currentPosition;
+        progress_details_number.text = "$currentPosition" + "/" + progressBar.max
+
+        question_id.text = question!!.question
+
+        image_id.setImageResource(question.image)
+        question_option_one.text = question.optionOne
+        question_option_two.text = question.optionTwo
+        question_option_three.text = question.optionThree
+        question_option_four.text = question.optionFour
+
     }
 }
